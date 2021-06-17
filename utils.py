8afff892af44248a0f2cae019139dc35636fba62
Kalabint <kalabint@debian-srv.local.kalabint.ch>
2021-06-17 17:59:31 +0200

    Fixing Print

--- utils.py
+++ utils.py
@@ -26,7 +26,7 @@ def load_config_vars():
      print(f"Temp Path not set, defaulting to /tmp/.")
      temp_path = "/tmp/"
 
-     print("Using the following Parameters: FFMPEG Threads: ", FFMPEG_THREADS, ", Temp Path: ", TMP_PATH, ", Telegram Bot Token: ", TELEGRAM_TOKEN, ".")
+     print('Using the following Parameters: FFMPEG Threads: ' + str(FFMPEG_THREADS) + ', Temp Path: ' + str(TMP_PATH) + ', Telegram Bot Token: ' + str(TELEGRAM_TOKEN) + '.')
      for variable in ["telegram_token", "ffmpeg_threads", "temp_path"]:
          config[variable] = eval(variable)
      return config
