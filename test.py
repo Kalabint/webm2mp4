9de921748dd0975d3c006a440197a80273c27fcb
Kalabint <kalabint@kalabint.com>
2021-07-19 08:01:56 +0000

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,8 @@
 # https://github.com/MikeWent/webm2mp4
 # https://t.me/webm2mp4bot
 
-import bot.py
+if __name__ == "__bot__":
+   # stuff only to run when not called via 'import' here
+   bot()
 
 print ("Test OK!");
